ba4f59c526fa6522db705abcb9995d45e272df21
Pranav Teegavarapu <pranavnt@outlook.com>
2021-08-29 19:32:50 -0700

    Initial Commit

---
+++ Shared/gnApp.swift
@@ -0,0 +1,17 @@
+//
+//  gnApp.swift
+//  Shared
+//
+//  Created by Pranav Teegavarapu on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct gnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
